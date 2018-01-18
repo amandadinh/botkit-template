72fc491f97c92ad3ffca9b8ea658ef5b43f797c3
amandadinh <35572095+amandadinh@users.noreply.github.com>
2018-01-18 11:18:41 -0800

    Create newuser.js

---
+++ skills/newuser.js
@@ -0,0 +1,33 @@
+module.exports = function (controller) {
+
+    controller.hears([/^newuser.js$/], "direct_message,direct_mention", function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+
+            convo.ask("Enter the full name of the new employee [First Last]", [
+                {
+                    var NewUserNameString = convo.extractResponse('fullname');
+                    var NewUserSplit = newUserNameString.split(' ', 2)
+                    var NewUserFirst = NewUserSplit[1];
+                    callback: function (response, convo) {
+                        convo.gotoThread("success");
+                    },
+                },
+                {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.say("Sorry, I don't know this color. Try another one...");
+                        convo.repeat();
+                        convo.next();
+                    }
+                }
+            ], { key: "answer" });
+
+            // Success thread
+            convo.addMessage(
+                "Cool, I love '{{responses.NewUserFirst}}' too",
+                "success");
+        });
+    });
+};
+
